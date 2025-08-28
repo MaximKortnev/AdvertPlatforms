7eb68f7333f10d9e8aa930bd3538df82207fd733
max <fafccc@gmail.com>
2025-08-28 13:51:09 +0300

    Добавил заготовку под сервис

---
+++ AdvertPlatforms.Api/Services/PlatformRepository.cs
@@ -0,0 +1,22 @@
+﻿using AdvertPlatforms.Api.Models;
+
+namespace AdvertPlatforms.Api.Services
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        public IEnumerable<AdvertisingPlatform> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UploadResult> LoadFromStreamAsync(Stream fileStream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> SearchByLocation(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
