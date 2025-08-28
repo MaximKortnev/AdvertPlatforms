7931bb638e71453568ef9358aa06071d988c5fb5
max <fafccc@gmail.com>
2025-08-28 16:00:33 +0300

    Описал program

--- AdvertPlatforms.Api/Program.cs
+++ AdvertPlatforms.Api/Program.cs
@@ -1,10 +1,34 @@
+using AdvertPlatforms.Api.Services;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Advertising Platforms API",
+        Version = "v1",
+        Description = "API для загрузки рекламных площадок и поиска по локации"
+    });
+});
+
+builder.Services.AddSingleton<IPlatformRepository, PlatformRepository>();
+
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Advertising Platforms API v1");
+    c.RoutePrefix = "swagger";
+});
+
 
 if (app.Environment.IsDevelopment())
 {
