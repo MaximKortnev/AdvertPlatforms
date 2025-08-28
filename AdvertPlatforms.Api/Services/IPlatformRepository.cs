851a4d426e1ec2850d64b1b39010b4cba06b93d3
max <fafccc@gmail.com>
2025-08-28 13:50:08 +0300

    Добавил интерфейс для сервиса

---
+++ AdvertPlatforms.Api/Services/IPlatformRepository.cs
@@ -0,0 +1,23 @@
+﻿using AdvertPlatforms.Api.Models;
+
+namespace AdvertPlatforms.Api.Services
+{
+    public interface IPlatformRepository
+    {
+        /// <summary>
+        /// Загружает данные из файла в память.
+        /// Бросает FormatException при проблемах с форматом.
+        /// </summary>
+        Task<UploadResult> LoadFromStreamAsync(Stream fileStream);
+
+        /// <summary>
+        /// Возвращает имена площадок, подходящие для локации.
+        /// </summary>
+        List<string> SearchByLocation(string location);
+
+        /// <summary>
+        /// Для отладки/расширения.
+        /// </summary>
+        IEnumerable<AdvertisingPlatform> GetAll();
+    }
+}
